2d509dc6d6682d45d06a29f044822ff40b4ba9c3
lighthoof <andrew.kuligin@gmail.com>
2026-04-19 22:09:10 +0300

    Implemented getFirstParagraphFromHTML with initial tests

--- get_data.go
+++ get_data.go
@@ -19,6 +19,18 @@ func getHeadingFromHTML(html string) (string, error) {
 	return tagContent.Text(), nil
 }
 
-func getFristParagraphFromHTML(html string) (string, error) {
-	return "", nil
+func getFirstParagraphFromHTML(html string) (string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		return "", err
+	}
+
+	tagContent := doc.Find("main")
+	if len(tagContent.Nodes) > 0 {
+		tagContent = tagContent.Find("p")
+	} else {
+		tagContent = doc.Find("p")
+	}
+
+	return tagContent.First().Text(), nil
 }
--- get_data_test.go
+++ get_data_test.go
@@ -96,3 +96,93 @@ func TestGetHeadingFromHTML(t *testing.T) {
 		})
 	}
 }
+
+func TestGetFirstParagraphFromHTML(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputURL      string
+		expected      string
+		errorContains string
+	}{
+		{
+			name: "find first p in main",
+			inputURL: `<html>
+						  <body>
+						  	<p>Whatever, skip this</p>
+						    <h1>Welcome to Boot.dev</h1>
+						    <main>
+						      <p>Learn to code by building real projects.</p>
+						      <p>This is the second paragraph.</p>
+						    </main>
+						  </body>
+						</html>`,
+			expected: "Learn to code by building real projects.",
+		},
+		{
+			name: "find first p without main",
+			inputURL: `<html>
+						  <body>
+						    <h1>Welcome to Boot.dev</h1>
+						    <p>Learn to code by building real projects.</p>
+						    <p>This is the second paragraph.</p>
+						  </body>
+						</html>`,
+			expected: "Learn to code by building real projects.",
+		},
+		{
+			name: "no closing tag",
+			inputURL: `<html>
+						  <body>
+						    <h1>Welcome to Boot.dev</h1>
+						    <main>
+						      <p>Learn to code by building real projects.</p
+						      <p>This is the second paragraph.</p>
+						    </main>
+						  </body>
+						</html>`,
+			expected: "Learn to code by building real projects.",
+		},
+		{
+			name: "no opening tag",
+			inputURL: `<html>
+						  <body>
+						    <h1>Welcome to Boot.dev</h1>
+						    <main>
+						      p>Learn to code by building real projects.</p>
+						      <p>This is the second paragraph.</p>
+						    </main>
+						  </body>
+						</html>`,
+			expected: "",
+		},
+		{
+			name: "not html",
+			inputURL: `if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return`,
+			expected: "",
+			//errorContains: "couldn't parse URL",
+		},
+		// add more test cases here
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := getFirstParagraphFromHTML(tc.inputURL)
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			} else if err != nil && tc.errorContains == "" {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			} else if err == nil && tc.errorContains != "" {
+				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', got none.", i, tc.name, tc.errorContains)
+				return
+			}
+
+			if actual != tc.expected {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
